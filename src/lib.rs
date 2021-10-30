0abda949a1088fad59e12ae0e2306499241ff989
Ptrskay3 <leeh123peter@gmail.com>
2021-10-30 10:46:55 +0200

    progress

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,29 @@
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpResponse, HttpServer};
+use serde::Deserialize;
 use std::net::TcpListener;
+
+#[derive(Deserialize)]
+#[allow(dead_code)]
+struct FormData {
+    name: String,
+    email: String,
+}
+
+async fn subscribe(_formdata: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
 
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    Ok(
-        HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-            .listen(listener)?
-            .run(),
-    )
+    Ok(HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscribe", web::post().to(subscribe))
+    })
+    .listen(listener)?
+    .run())
 }
